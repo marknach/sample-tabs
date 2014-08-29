9e53e60924d0f62e3fe4f3431713f806fbba4995
Mark Nachazel <marknach@vt.edu>
2014-08-29 14:36:04 -0600

    finish new tab functionality

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ $(document).ready(function() {
     $('#' + $(this).data('tab')).show();
   });
   $('.add-tab-button').click(function() {
-    $(this).before($("<span class='tab-button'></span>"));
+    $('.tab-button').removeClass('active');
+    $('.content').hide();
+    var newTab = $("<li><a class='tab-button active'>New Tab</a></li>");
+    var newContent = $("<div class='content'>New Tab content</div>");
+    $('.content-container').append(newContent);
+    newTab.click(function() {
+      $('.tab-button').removeClass('active');
+      $('.content').hide();
+      newContent.show();
+      newTab.find("a").addClass("active");
+    });
+    $(this).parent().before($(newTab));
   });
 });
